e6848ea7ac55a8c60b4bfecfc5864c1cc947a275
Ian Salmons <ian.salmons@gmail.com>
2021-12-04 15:00:48 +0000

    Formatting changes

--- advent-of-code-2021/day_2/test_day_2.py
+++ advent-of-code-2021/day_2/test_day_2.py
@@ -1,14 +1,21 @@
 from .day_2 import part_1, part_2
 
-TEST_DATA = [('forward', 5), ('down', 5), ('forward', 8), ('up', 3), ('down', 8), ('forward', 2)]
+TEST_DATA = [
+    ("forward", 5),
+    ("down", 5),
+    ("forward", 8),
+    ("up", 3),
+    ("down", 8),
+    ("forward", 2),
+]
 
 
 def load_data():
     def fmt(l: str):
-        dir, amt = l.split(' ')
+        dir, amt = l.split(" ")
         return dir, int(amt)
 
-    with open('advent-of-code-2021/day_2/data') as data:
+    with open("advent-of-code-2021/day_2/data") as data:
         return [fmt(line.strip()) for line in data]
 
 
