abd71a236e9f08f58a24d91c1a29364b52e73894
Ian Salmons <ian.salmons@gmail.com>
2021-12-04 15:25:52 +0000

    Formatting changes

--- advent-of-code-2021/day_3/day_3.py
+++ advent-of-code-2021/day_3/day_3.py
@@ -2,28 +2,28 @@ import collections
 
 
 def part_1(bytes):
-    gamma, epsilon = '', ''
+    gamma, epsilon = "", ""
     for i in range(len(bytes[0])):
         c = collections.Counter([byte[i] for byte in bytes]).most_common()
-        gamma += (c[0][0])
-        epsilon += (c[-1][0])
+        gamma += c[0][0]
+        epsilon += c[-1][0]
     return int(gamma, 2) * int(epsilon, 2)
 
 
 def part_2(bytes):
     diag = {
-        'oxy_bytes': [1, bytes],
-        'co2_bytes': [0, bytes],
+        "oxy_bytes": [1, bytes],
+        "co2_bytes": [0, bytes],
     }
     for i in range(len(bytes[0])):
         for k, (typ, bytes) in diag.items():
             if len(bytes) > 1:
                 c = collections.Counter([byte[i] for byte in bytes]).most_common()
                 if c[0][1] != c[-1][1]:
-                    keep = c[typ-1][0]
+                    keep = c[typ - 1][0]
                 else:
                     keep = str(typ)
                 diag[k][1] = [b for b in bytes if b[i] == keep]
-    oxy_rating = int(diag['oxy_bytes'][1][0], 2)
-    co2_rating = int(diag['co2_bytes'][1][0], 2)
+    oxy_rating = int(diag["oxy_bytes"][1][0], 2)
+    co2_rating = int(diag["co2_bytes"][1][0], 2)
     return oxy_rating * co2_rating
